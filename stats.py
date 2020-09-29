6c1a45feb75420a3ba896882773778e5c54b45f1
Pepi <pepimartinez@protonmail.com>
2020-09-29 12:09:43 -0400

    changed data project

--- stats.py
+++ stats.py
@@ -1,5 +1,18 @@
+import csv
 import os
 path = os.path.dirname(os.path.abspath(__file__))
-full_path_in = os.path.join(path, 'Air_Quality.csv')
+full_path_in = os.path.join(path, 'New_York_City_Leading_Causes_of_Death.csv')
 
-print(full_path_in)
+# Open the data file
+def open_data_file():
+    with open(full_path_in, 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            print(row[-1])
+# Create data struct for the data
+
+
+# Do stats on that data
+
+
+open_data_file()
