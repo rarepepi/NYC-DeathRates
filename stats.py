17d74ce790c4fd28cac55aee9578af1442362c85
Pepi <pepimartinez@protonmail.com>
2020-09-29 16:24:24 -0400

    renamed function that are not to be called by user

--- stats.py
+++ stats.py
@@ -19,34 +19,39 @@ class DeathDataContainer:
         return f"{self.year} |  {self.cause} | {self.sex} | {self.race} | {self.deaths} | {self.rate} | {self.age_adj_rate}"
 
 
-def is_valid_row(row):
+# Open the data file
+def open_data_file():
+    with open(full_path_in) as csv_file:
+        reader = csv.reader(csv_file, delimiter=',')
+        for row in reader:
+            _process_row(row)
+
+# Clean the data and create data struct
+def _is_valid_row(row):
     if all(v != "." for v in row):
         return True
     return False
 
-
-def process_row(row):
-    if is_valid_row(row):
+def _process_row(row):
+    if _is_valid_row(row):
         container = DeathDataContainer(
             row[0], row[1], row[2],
             row[3], row[4], row[5],
             row[6])
         cleaned_data.append(container)
 
-
-# Open the data file
-def open_data_file():
-    with open(full_path_in) as csv_file:
-        reader = csv.reader(csv_file, delimiter=',')
-        for row in reader:
-            process_row(row)
-
-# Create data struct for the data
+# Do stats on the cleaned data
+def find_mean(data):
+    return sum(data) / len(data)
 
 
-# Do stats on that data
+def print_all_data():
+    for d in cleaned_data:
+        print(d)
 
+# ------------ MAIN ------------
+def main():
+    open_data_file()
 
-open_data_file()
-for d in cleaned_data:
-    print(d)
+if __name__ == '__main__':
+    main()
